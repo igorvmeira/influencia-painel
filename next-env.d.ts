a576f294eae252fad4a9a02fe63bae98bb2a8921
igorvmeira <igorvmeira@gmail.com>
2026-06-25 12:39:39 -0300

    Add files via upload

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
-node_modules
-.next
-.env
-.env*.local
-.DS_Store
-*.log
-.vercel
-next-env.d.ts
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
